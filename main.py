ee0ec6dfe42ace23fe397ac4a41ed2ee920f75e6
PetePhuaphan <pete.ptpn@gmail.com>
2023-11-20 23:53:19 -0500

    update code

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from langchain.prompts import (
 from langchain.prompts.prompt import PromptTemplate
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
+import streamlit.components.v1 as components
+import urllib, base64
 
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
@@ -32,6 +34,9 @@ index = Pinecone.from_existing_index(index_name, model)
 
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY)
 
+
+welcome_message = "Hello and welcome! 👋 I'm the AI chatbot representation of Tuyen Nguyen, but you can call me Tracy. I'm here to provide you with interactive insights into my education, skills, and work experiences. Feel free to ask me anything related to my professional journey or specific abilities. Whether you're curious about my academic background, the projects I've worked on, or the skills I've honed along the way, I'm here to answer your questions. Let's make this conversation informative and engaging! 🌟"
+
 system_msg_template = SystemMessagePromptTemplate.from_template(template="""
 SYSTEM: You are a RAG chatbot acting as an interactive resume. You provide information about work experiences, educational background, skills, and achievements based on the resume and LinkedIn profile. Answer the questions in first-person statements.
 
@@ -47,6 +52,8 @@ Do not try to make up an answer:
 
 human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
 
+
+
 prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
 memory = ConversationBufferWindowMemory(k=3,return_messages=True)
 conversation = ConversationChain(memory=memory, prompt=prompt_template, llm=llm, verbose=True)
@@ -60,16 +67,30 @@ def add_to_conversation(entry):
     st.session_state.conversationmemory.append(entry)
     st.session_state.conversationmemory = st.session_state.conversationmemory[-3:]
 
+def input_templete(context,prompt,conversation_memory):
+    return f"""
+{conversation_memory}\n
+=============
+{context}
+=============
+
+Question: {prompt}
+Helpful Answer:
+"""
+
 
 st.subheader("Interactive Resume")
 
 # Initialize conversationmemory
 if 'conversationmemory' not in st.session_state:
     st.session_state.conversationmemory = []
+    
+
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    st.session_state.messages.append({"role": "assistant", "content": welcome_message})
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -82,7 +103,7 @@ conversation_memory = ""
 for entry in newest_entries:
     conversation_memory += entry + "\n"
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Message me"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
@@ -96,17 +117,10 @@ if prompt := st.chat_input("What is up?"):
             context = find_match(prompt)
             print(f"prompt : {prompt}")
             #response = conversation.predict(input=f"{conversation_memory}Context:\n{context}\n\nQuery:\n{prompt}")
-            response = conversation.predict(input=f"""
-{conversation_memory}\n
-=============
-{context}
-=============
-
-Question: {prompt}
-Helpful Answer:
-""")
+            response = conversation.predict(input=input_templete(context,prompt,conversation_memory))
             print(response)
             st.markdown(response)
+            
     add_to_conversation(f"""
 =============
 {context}
